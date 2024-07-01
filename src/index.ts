da0fa9c1b9d2081fcc4a84561f4d909cc1b12ee9
akinocccc <akinochen@foxmail.com>
2024-07-01 23:57:13 +0800

    wip: eslint config generator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-const eslint = ({rules: EslintRules} = {}) => {
-    return {
 
-    }
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue'
+
+const eslintConfig = () => {
+    return [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...pluginVue.configs['flat/recommended'],
+      {
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+        },
+        rules: {
+          // override/add rules settings here, such as:
+        }
+      }
+    ]
 }
+
+export default eslintConfig;
